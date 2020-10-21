4ece2a0bc1af8a6891654cf315a706437df3e302
msunkist99 <msunkist99@gmail.com>
2020-10-21 13:19:37 -0500

    step 3 completed

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-list',
@@ -11,5 +12,7 @@ export class OrbitListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  @Input() satellites: Satellite[];
 
 }
